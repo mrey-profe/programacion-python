922b3f072cba85d969d0687f2bc4dcd4a99438e2
Marta Rey <marta.rey@edu.xunta.gal>
2023-10-06 09:49:35 +0200

    Resueltos ejercicios bucles salvo anidados

--- 02Estructuras_de_control/codigo/ejemplo_while.py
+++ 02Estructuras_de_control/codigo/ejemplo_while.py
@@ -1,4 +1,11 @@
-for i in range(4):
-    for j in range(4):
-        print(i, j, sep = "", end=" ")
-    print()
+hay_negativos = False
+
+for i in range(10):
+    numero = int(input("Escribe un número: "))
+    if (numero < 0):
+        hay_negativos = True
+
+if hay_negativos: # if hay_negativos == True
+    print("Hay negativos")
+else:
+    print("No hay negativos")
