78fdc4098b4c231994aa63bbb06310d66edf3b34
Marta Rey <marta.rey@edu.xunta.gal>
2024-02-02 16:33:24 +0100

    Eliminados no apuntes

--- pruebas/prueba.py
+++
@@ -1,6 +0,0 @@
-# Vamos a hacer un programa ambientado en los libros de Harry Potter
-
-magos = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore', 'Severus Snape', 'Lord Voldemort']
-casas = ['Gryffindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin']
-
-fechas_nacimiento = [1980, 1979, 1980, 1881, 1960, 1926]
