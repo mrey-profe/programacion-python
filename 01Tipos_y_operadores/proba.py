eb1088a9dca5ab890c3323c1b47b21c1aa833009
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-04 14:27:49 +0200

    Modificaciones for

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,10 +1,3 @@
-año = int(input("Dime un año: "))
-resto = año % 4
-resto_bisiesto = resto % 400
-if resto == 0:
-    if resto_bisiesto == 0:
-        print("El año es bisiesto")
-    else:
-        print("El año no es bisiesto")
-else:
-    print("El año no es bisiesto")
+palabra =  "Hola"
+for letra in palabra:
+    print(letra, end="-")
