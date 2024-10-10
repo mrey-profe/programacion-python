07179988ea05726245187f0030128d09931e8310
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-10 13:52:08 +0200

    Todas soluciones while y for hasta 5

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,9 +1,14 @@
-suma = 0
-numero = 0
+print("Introduce tu fecha de nacimiento:")
+dia = int(input("Día: "))
+mes = int(input("Mes: "))
+anho = int(input("Año: "))
 
-while suma < 200000:
-    numero = numero + 1
-    suma = suma + numero # suma += numero
-    
+suma = dia + mes + anho
+num_suerte = 0
 
-print(numero)
+while suma != 0:
+    cifra = suma % 10
+    suma = suma // 10
+    num_suerte += cifra # num_suerte = num_suerte + cifra
+
+print(num_suerte)
