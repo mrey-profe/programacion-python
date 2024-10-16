86c97b0c3bcad9bf450bd6c386ea63e0b54b1ec5
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-16 10:01:10 +0200

    Añadido qué ejercicios de pirámides son fáciles.

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,14 +1,4 @@
-print("Introduce tu fecha de nacimiento:")
-dia = int(input("Día: "))
-mes = int(input("Mes: "))
-anho = int(input("Año: "))
-
-suma = dia + mes + anho
-num_suerte = 0
-
-while suma != 0:
-    cifra = suma % 10
-    suma = suma // 10
-    num_suerte += cifra # num_suerte = num_suerte + cifra
-
-print(num_suerte)
+for piso in range(1,5):
+    for letra in "ABCD":
+        print(piso, letra, sep = "º", end=" ")
+    print()
