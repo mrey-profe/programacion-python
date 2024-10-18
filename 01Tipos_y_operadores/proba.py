52b150846b308fcb31cebfd2ed358a507a122d3a
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-18 14:26:41 +0200

    Añadido ejemplo tabla de multilplicar en funciones

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,11 +1,5 @@
-numero_max = int(input("Escribe un número y te diré cuántos primos menores que él existen: "))
-contador = 0
+def saludar(nombre):
+    print("Hola,", nombre)
 
-for numero in range(1, numero_max + 1):
-    es_primo = True
-    for i in range(2, numero):
-        if numero % i == 0: #É divisible
-            es_primo = False
-            break
-    if es_primo:
-        contador += 1
+nombre = input("¿Cómo te llamas? ") # No tiene por qué llamarse igual, pero puede hacerse ÁMBITO
+saludar("Ana")
