044b2d86a2d287f22aba443a47028b2d54f8acf3
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-18 13:29:01 +0200

    Resueltos ejercicios repetitivas

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,4 +1,11 @@
-for piso in range(1,5):
-    for letra in "ABCD":
-        print(piso, letra, sep = "º", end=" ")
-    print()
+numero_max = int(input("Escribe un número y te diré cuántos primos menores que él existen: "))
+contador = 0
+
+for numero in range(1, numero_max + 1):
+    es_primo = True
+    for i in range(2, numero):
+        if numero % i == 0: #É divisible
+            es_primo = False
+            break
+    if es_primo:
+        contador += 1
