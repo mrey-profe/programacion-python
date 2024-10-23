1d0045f05bc29f850e8348b23d78ea57075035b0
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-23 12:37:37 +0200

    Añadido ejercicio 1a funciones

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,5 +1,7 @@
-def saludar(nombre):
-    print("Hola,", nombre)
+import math
 
-nombre = input("¿Cómo te llamas? ") # No tiene por qué llamarse igual, pero puede hacerse ÁMBITO
-saludar("Ana")
+def area_circulo(radio):
+    area = math.pi * radio ** 2
+    return area
+
+print(round(area_circulo(10), 2))
