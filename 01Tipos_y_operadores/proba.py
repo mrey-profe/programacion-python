5ee24a3ee3019b6ac33cc7b29650ea21a4d3a3df
Marta Rey López <marta.rey@edu.xunta.gal>
2024-10-30 12:54:37 +0100

    Ejercicios funciones resueltos

--- 01Tipos_y_operadores/proba.py
+++ 01Tipos_y_operadores/proba.py
@@ -1,7 +1,8 @@
-import math
+def piramide(altura: int) -> None:
+    for fila in range(1, altura + 1): # Filas
+        print(" " * (altura - fila), end="")
+        for columna in range(fila, 0, -1): #Columnas
+            print(columna, end="")
+        print()
 
-def area_circulo(radio):
-    area = math.pi * radio ** 2
-    return area
-
-print(round(area_circulo(10), 2))
+piramide(5)
