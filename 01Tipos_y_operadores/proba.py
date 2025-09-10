d8c5d3c1c7124bb8e0b5da2d1407cc1f32afdf14
Marta Rey López <marta.rey@edu.xunta.gal>
2025-09-10 13:29:09 +0200

    Inicio curso 2025-2026

--- 01Tipos_y_operadores/proba.py
+++
@@ -1,8 +0,0 @@
-def piramide(altura: int) -> None:
-    for fila in range(1, altura + 1): # Filas
-        print(" " * (altura - fila), end="")
-        for columna in range(fila, 0, -1): #Columnas
-            print(columna, end="")
-        print()
-
-piramide(5)
