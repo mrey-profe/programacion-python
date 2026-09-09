b429c8d484aaa49ba35efa61c430858d16fbb9d9
Marta Rey López <marta.rey@edu.xunta.gal>
2026-09-09 23:15:59 +0200

    Inicio curso 2026-2027

--- 07Ficheros/solucións/5.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ficheiros = os.listdir(".")
-
-with open("directory.txt", "w") as ficheiro_directorio:
-    for ficheiro in ficheiros:
-        ficheiro_directorio.write(ficheiro + "\n")
